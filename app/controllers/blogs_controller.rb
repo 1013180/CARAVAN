97a83e805ff67ae60d45db92808ef8849e9dd3e6
1013180 <hiroki.kojima0417@gmail.com>
2021-01-09 11:59:25 +0000

    [Finish]3-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -27,9 +27,15 @@ class BlogsController < ApplicationController
     redirect_to blogs_path(blog)
   end
 
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end
+
+
   private
   def blog_paramas
-    params.require(:blog).permit(:title, :category, :body)
-end
+    params.require(:blog).permit(:title, :category, :body)end
 
 end
